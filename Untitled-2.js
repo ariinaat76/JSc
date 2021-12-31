1e8c43325c818ea6b8c8707bdf8886dc0d7e9314
ariinaat76 <93032496+ariinaat76@users.noreply.github.com>
2021-12-31 19:26:05 +0200

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,16 @@
+var x = prompt("Решите пример: 7+9(9-3)/3");
+x = parseInt(x);
+
+
+function task1 (x){
+    if(x == 25){
+        alert("Правильный ответ!")
+    }
+    else{
+        alert("Ошибка, попробуй еще раз")
+    }
+}
+task1(x)
+task1(x)
+
+
